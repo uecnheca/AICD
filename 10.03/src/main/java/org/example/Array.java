3ad9ec9e398e0a3641ef86f6418ff5d3b646e1a9
Анастасия <uecnheca@gmail.com>
2025-05-16 22:17:10 +0300

    Homework 26.04.25

--- 10.03/src/main/java/org/example/Array.java
+++ 10.03/src/main/java/org/example/Array.java
@@ -1,4 +1,38 @@
-package org.example;
+import java.util.*;
 
 public class Array {
+    public static void main(String[] args) {
+        int[] nums1 = {2, 4, 4, 1};
+        int[] nums2 = {2, 2, 4, 5};
+
+        int[] result = func(nums1, nums2);
+        System.out.println(Arrays.toString(result)); // [2, 4]
+    }
+
+    public static int[] func(int[] nums1, int[] nums2) {
+        // 1. Создаем частотные карты для обоих массивов
+        Map<Integer, Integer> arr1 = createFrequencyMap(nums1);
+        Map<Integer, Integer> arr2 = createFrequencyMap(nums2);
+
+        Set<Integer> keys = new HashSet<>(arr1.keySet());
+        keys.retainAll(arr2.keySet());
+
+        List<Integer> result = new ArrayList<>();
+        for (int key : keys) {
+            int count = Math.min(arr1.get(key), arr2.get(key));
+            for (int i = 0; i < count; i++) {
+                result.add(key);
+            }
+        }
+
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+
+    private static Map<Integer, Integer> createFrequencyMap(int[] nums) {
+        Map<Integer, Integer> Map = new HashMap<>();
+        for (int num : nums) {
+            Map.put(num, Map.getOrDefault(num, 0) + 1);
+        }
+        return Map;
+    }
 }
