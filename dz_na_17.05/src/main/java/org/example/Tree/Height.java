7ef57e071844da2cd1673d7ce2e7babe1ce73d6f
Анастасия <uecnheca@gmail.com>
2025-05-16 22:57:26 +0300

    Homework 16.05.25

---
+++ dz_na_17.05/src/main/java/org/example/Tree/Height.java
@@ -0,0 +1,24 @@
+package org.example.Tree;
+
+public class Height {
+    public static int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+
+        int height = getHeight(root);
+        System.out.println("Высота дерева: " + height);
+    }
+}
